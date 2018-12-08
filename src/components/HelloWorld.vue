3157731b5e4787908350a8814b9c6c151ac93917
Robert Forster <robert.forster@beamvex.co.uk>
2018-12-08 18:26:13 +0000

    init commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container>
+    <v-layout
+      text-xs-center
+      wrap
+    >
+      <h1>{{message}}</h1>
+
+      <v-btn color="success" @click="clickhere">click here</v-btn>
+
+      {{couldbe}}
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+
+import Store from '../model/store'
+  export default {
+    props: {
+      couldbe: String
+    },
+    computed: {
+    message () {
+      return Store.state.count
+    }
+  },
+    methods: {
+      clickhere() {
+        
+          this.object1.message ='yup';
+          
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
